f161974e4f9ce8c745137fa005b1ae328866a77f
purohit.pratik405@gmail.com <purohit.pratik405@gmail.com>
2020-04-17 17:30:11 +0530

    Master

---
+++ Encryptionhandling/src/main/java/com/encryption/Encryptionhandling/EncryptionhandlingApplication.java
@@ -0,0 +1,13 @@
+package com.encryption.Encryptionhandling;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EncryptionhandlingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EncryptionhandlingApplication.class, args);
+	}
+
+}
---
+++ Encryptionhandling/src/test/java/com/encryption/Encryptionhandling/EncryptionhandlingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.encryption.Encryptionhandling;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EncryptionhandlingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
